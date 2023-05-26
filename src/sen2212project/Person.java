cfde64deb046a11ae2b919086f374d2a607b9d6c
maozkurt <maozkurt12@gmail.com>
2023-05-26 18:03:32 +0300

    more implementation

--- src/sen2212project/Person.java
+++ src/sen2212project/Person.java
@@ -2,21 +2,26 @@ package sen2212project;
 
 public class Person {
 	private String name;
-	private int height; //in cm
+	private String height; //in cm
 	private String eyeColor;
+	private String hairColor;
 	
-	public Person(String name, int height, String eyeColor) {
+	public Person(String name, String height, String eyeColor, String hairColor) {
 		this.name = name;
 		this.height = height;
 		this.eyeColor = eyeColor;
+		this.hairColor = hairColor;
 	}
 	public String getName() {
 		return name;
 	}
-	public int getHeight() { 
+	public String getHeight() { 
 		return height;
 	}
-	public String eyeColor() {
+	public String getEyeColor() {
 		return eyeColor;
+	}
+	public String getHairColor() {
+		return hairColor;
 	}
 }
